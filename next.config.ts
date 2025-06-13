0afcdda50b8befd59783fb671f2e7a2c87bfa977
local-Leee <locallee.c@gmail.com>
2025-06-13 15:44:59 +0900

    노션 이미지 안나오는거 수정

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
             {
                 hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
             },
+            {
+                hostname: "www.notion.so",
+            },
         ],
     },
 };
